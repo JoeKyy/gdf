91c30a067dd6a1cf040e43fd92748375325c9483
Jhomar Nando <contato@joekyy.com.br>
2020-12-15 22:05:48 -0300

    Iniciando o projeto

---
+++ app/js/jquery.functions.js
@@ -0,0 +1,38 @@
+$(function () {
+	$('.highlight--slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		  ]
+	});
+
+	$('.nav-button').click(function(){
+		if (!$(this).hasClass('is-active')) {
+			$(this).addClass('is-active');
+			$('.nav-menu').addClass('nav-menu--open');
+			$('.nav-menu').removeClass('nav-menu--close');
+		} else {
+			$(this).removeClass('is-active');
+			$('.nav-menu').removeClass('nav-menu--open');
+			$('.nav-menu').addClass('nav-menu--close');
+		}
+	});
+});
